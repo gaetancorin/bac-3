63ab82ad05983085075e7570a73326462840f6f2
gaetancorin <gaetancorin@gmail.com>
2023-01-18 15:10:33 +0100

    API java + aide github

--- architecture distribué/axemple/src/main/java/com/example/axemple/ressources/EtudiantRessource.java
+++ architecture distribué/axemple/src/main/java/com/example/axemple/ressources/EtudiantRessource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("Etudiant")
@@ -40,7 +41,39 @@ public class EtudiantRessource {
         int age = Period.between(Dto.getDate_naissance(), LocalDate.now()).getYears();
         String message = "je m'appelle "+Dto.getPrenom()+" "+Dto.getNom()+" j'ai "+age+" ans";
         return new ResponseEntity<>(message, HttpStatus.OK);
+    }
 
+    @GetMapping("Example6")
+    public ResponseEntity<String> example6(@RequestBody Double[] arrayDouble){
+        Double total = 0.0;
+        for (int i=0; i<arrayDouble.length; i++) {
+            total += arrayDouble[i];
+        }
+        Double moyenne = total / arrayDouble.length;
+        String message = "Le total de la liste est "+total+" et sa moyenne est de "+moyenne;
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    @GetMapping("Example7")
+    public ResponseEntity<String> example7(@RequestBody Double[] arrayDouble, @RequestParam String maxOrMin){
+        Double valMax = arrayDouble[0];
+        Double valMin = arrayDouble[0];
+        for (int i=1; i<arrayDouble.length; i++) {
+            if(arrayDouble[i] > valMax){
+                valMax = arrayDouble[i];
+            }
+            if(arrayDouble[i] < valMin){
+                valMin = arrayDouble[i];
+            }
+        }
+        String message = "Il faut donner un paramètre max ou min";
+        if(Objects.equals(maxOrMin, "max")){
+            message = "La valeur max du tableau est "+ valMax;
+        }
+        if(Objects.equals(maxOrMin, "min")){
+            message = "La valeur min du tableau est "+ valMin;;
+        }
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
 }
